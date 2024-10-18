02b46004e397db3b5f7a57ed54b7c08e3ad54eba
Dmytro <datsenkodev@gmail.com>
2024-10-18 20:54:43 +0300

    Fix conflict

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -13,18 +13,19 @@ import arrowUp from '../assets/icons/arrow-up.svg';
 const CategoryPage = () => {
   const [categoryId, setCategoryId] = useState(null);
   const filters = useSelector((state) => state.categryFilter);
-  const [arrivals, setArrivals] = useState([]);
-  const [page, setPage] = useState(1);
-  const [next, setNext] = useState('true');
-  const [totalItems, setTotalItems] = useState(0);
-  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
-  const perPage = 8;
   let query = {};
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   const handleTogglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
   };
+
+  const [arrivals, setArrivals] = useState([]);
+
+  const [page, setPage] = useState(1);
+  const [next, setNext] = useState('true');
 
   // fetch product
   const fetchData = async (page_size, page) => {
@@ -43,8 +44,6 @@ const CategoryPage = () => {
       let queryString = Object.values(query).join('');
       console.log(queryString);
       const response = await $api.get(`/api/products/?page_size=${page_size}&page=${page}${queryString}`);
-      setTotalItems(response.data.count);
-
       setNext(response.data.next);
       // setArrivals((currentArrivals) => [...currentArrivals, ...response.data.results]);
       setArrivals(response.data.results);
@@ -56,12 +55,11 @@ const CategoryPage = () => {
   };
 
   useEffect(() => {
-    fetchData(perPage, page);
+    fetchData(8, page);
   }, [page, filters]);
 
   // Pagination
-  const pages = [];
-  const totalPages = Math.ceil(totalItems / perPage);
+  const pages = [1, 2, 3, 4, 5];
 
   const setNewPage = (direction) => {
     if (page !== 1 && direction === -1) {
@@ -83,14 +81,22 @@ const CategoryPage = () => {
       {arrivals.length > 0 ? (
         <>
           <div className='pagination flex justify-center items-center py-10 my-12'>
-            <Button onClickBtn={() => setNewPage(-1)} disabled={page === 1 ? true : false}>
+            <Button onClickBtn={() => setNewPage(-1)}>
               <img src={arrowUp} alt='previous page button' className={`rotate-[270deg] ${page === 1 ? 'invert cursor-default' : ''}`} />
             </Button>
-
-            <Pagination totalPages={totalPages} page={page} setPage={setPage} />
-
-            <Button onClickBtn={() => setNewPage(+1)} className='bg-black ' disabled={page === totalPages ? true : false}>
-              <img src={arrowUp} alt='next page button' className={`rotate-90 ${page === totalPages ? 'invert cursor-default' : ''}`} />
+            <ul className='flex gap-5 mx-[100px]'>
+              {pages.map((pageBtn) => {
+                return (
+                  <li>
+                    <Button key={pageBtn.id} classNameBtn={`text-xl font-sans ${pageBtn === page ? 'text-black' : 'text-gray'}`} onClickBtn={() => setPage(pageBtn)}>
+                      {pageBtn}
+                    </Button>
+                  </li>
+                );
+              })}
+            </ul>
+            <Button onClickBtn={() => setNewPage(+1)}>
+              <img src={arrowUp} alt='next page button' className={`rotate-90 ${page === pages.length ? 'invert cursor-default' : ''}`} />
             </Button>
           </div>
 
