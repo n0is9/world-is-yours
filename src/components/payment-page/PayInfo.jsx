8c1b8d26b2171af0701aba8ba216714dce40eee8
Nataliia-Homyk324 <homyk324@gmail.com>
2024-11-01 09:55:42 +0200

    cart

--- src/components/payment-page/PayInfo.jsx
+++ src/components/payment-page/PayInfo.jsx
@@ -13,32 +13,56 @@ const PayInfo = () => {
       {/* ДЛЯ НОВОГО ПОКУПЦЯ */}
 
       <div className='flex flex-col'>
-        <label className='text-textLight font-medium font-raleway text-sm mb-2'>Варіант оплати</label>
+        <label className='text-textLight font-medium font-raleway text-sm mb-2'>
+          Варіант оплати
+        </label>
         <form className='flex flex-col gap-2'>
           <div className='flex items-center gap-2'>
-            <input type='radio' id='opt1' name='opt1' value='opt1' className='h-4 w-4' onChange={() => handleOptionChange('opt1')} />
+            <input
+              type='radio'
+              id='opt1'
+              name='opt1'
+              value='opt1'
+              className='h-4 w-4'
+              onChange={() => handleOptionChange('opt1')}
+            />
             <label for='opt1' className='text-base'>
-              {' '}
               Готівка
             </label>
           </div>
           <div className='flex items-center gap-2'>
-            <input type='radio' id='opt2' name='opt1' value='opt2' className='h-4 w-4' onChange={() => handleOptionChange('opt1')} />
+            <input
+              type='radio'
+              id='opt2'
+              name='opt1'
+              value='opt2'
+              className='h-4 w-4'
+              onChange={() => handleOptionChange('opt1')}
+            />
             <label for='opt2' className='text-base'>
-              {' '}
               Оплата банківською картою
             </label>
           </div>
           <div className='flex items-center gap-2'>
-            <input type='radio' id='opt3' name='opt1' value='opt3' className='h-4 w-4' onChange={() => handleOptionChange('opt1')} />
+            <input
+              type='radio'
+              id='opt3'
+              name='opt1'
+              value='opt3'
+              className='h-4 w-4'
+              onChange={() => handleOptionChange('opt1')}
+            />
             <label for='opt3' className='text-base'>
-              {' '}
               Google Pay
             </label>
           </div>
         </form>
-        <Button classNameBtn='max-w-md bg-gray-dark my-12 p-4 border rounded-xl leading-none font-bold text-20px text-white duration-300 hover:bg-transparent hover:text-black focus:bg-transparent focus:text-black' nameBtn='submitForm' valueBtn='submit'>
-          Оплатити
+        <Button
+          classNameBtn='max-w-md bg-gray-dark my-12 p-4 border rounded-xl leading-none font-bold text-20px text-white duration-300 hover:bg-transparent hover:text-black focus:bg-transparent focus:text-black'
+          nameBtn='submitForm'
+          valueBtn='submit'
+        >
+          Оформити замовлення
         </Button>
       </div>
 
